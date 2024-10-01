93a56205a43dc998af1a29863555632c03af0ce2
Edwin Rajesh <edwinrajesh2000@gmail.com>
2024-10-01 12:55:06 +0530

    Add files via upload

---
+++ Questions.py
@@ -0,0 +1,71 @@
+# # Equal
+
+# num= int(input("Enter a n8umber\n"))
+# if (num%2==0):
+#     print("The number is even\n")
+# else:
+#     print("The number is odd\n")   
+
+
+
+# # Even or Odd
+# num= int(input("Enter a n8umber\n"))
+
+# if (num%2==0):
+#     print("The number is even\n")
+# else:
+#     print("The number is odd\n")   
+
+
+
+# # Positive or Negative
+# num= int(input("Enter a number\n"))   
+
+# if (num>0):
+#     print("The number is positive\n")
+# elif(num<0):
+#     print("The number is negative\n")  
+# else:
+#     print("The number is 0\n")      
+
+
+
+# # Leap year
+# year= int(input("Enter the year\n"))
+
+# if (year%4 ==0):
+#     print("The number is a leap year\n")
+# else:
+#     print("The number isn't a leap year\n")
+
+
+
+
+# # Eligible to Vote
+# num= int(input("Enter the age\n"))   
+
+# if (num>=18):
+#     print("Person is eligible to vote\n")
+# else:
+#     print("Person is not eligible to vote\n")
+
+
+
+
+
+# Height Category
+# hght= int(input("Enter height in CM"))
+
+# if hght>180:
+
+
+
+# m= int(input("Enter the integer\n"))   
+# if (m>0):
+#     n=1
+# elif (m<0):
+#     n=-1
+# else:
+#     n=0
+
+# print(n)
